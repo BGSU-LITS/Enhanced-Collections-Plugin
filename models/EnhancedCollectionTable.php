07987ffbaa8db1aa571fc7893ca3e889990e0298
Dave Widmer <dwidmer@bgsu.edu>
2013-01-31 16:52:00 -0500

    Adding an enhanced collections table.

---
+++ models/EnhancedCollectionTable.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * The Enhanced Collection Table
+ *
+ * @package  Enhanced Collection
+ * @author   Dave Widmer <dwidmer@bgsu.edu>
+ */
+class EnhancedCollectionTable extends Omeka_Db_Table
+{
+
+}
