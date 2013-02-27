e7771a5cff27cdcbe8bad1f619d4e28edd83f700
Dave Widmer <dwidmer@bgsu.edu>
2013-02-27 17:00:28 -0500

    More updates the to the random/recent items in a collection.

--- functions.php
+++ functions.php
@@ -39,3 +39,35 @@ function get_random_featured_items_in_collection($id, $num = 5)
 	$table = new ItemTable('Item', get_db());
 	return $table->findRandomFeatured($id, $num);
 }
+
+/**
+ * Return HTML for random featured items.
+ *
+ * @package Omeka\Function\View\Body
+ * @uses get_random_featured_items()
+ * @param int $id      The collection id
+ * @param int $count   The number of items to feature
+ * @return string
+ */
+function random_featured_items_in_collection($id, $count = 5)
+{
+	return get_view()->partial(
+		'items/random-featured.php',
+		array('items' => get_random_featured_items_in_collection($id, $count), 'count' => $count)
+	);
+}
+
+/**
+ * Get the most recently added items in a given collection.
+ *
+ * @package Omeka\Function\View\Body
+ * @uses Table_Item::findBy()
+ * @param int     $id  The collection id
+ * @param integer $num The maximum number of recent items to return
+ * @return array
+ */
+function get_recent_items_in_collection($id, $num = 10)
+{
+	$table = new ItemTable('Item', get_db());
+	return $table->findRecentInCollection($id, $num);
+}
--- models/ItemTable.php
+++ models/ItemTable.php
@@ -7,9 +7,9 @@ class ItemTable extends Table_Item
 	 *
 	 * @param  int  $id  The collection id
 	 * @param  int  $num The number of featured items to find
-	 * @return           Result set
+	 * @return array         Result set
 	 */
-	public function findRandomFeatured($id, $limit = 5, $page = null)
+	public function findRandomFeatured($id, $limit = 5)
 	{
 		$params = array(
 			'featured' => 1, 
@@ -19,7 +19,30 @@ class ItemTable extends Table_Item
 
 		$select = $this->getSelectForFindBy($params);
 		if ($limit) {
-			$this->applyPagination($select, $limit, $page);
+			$this->applyPagination($select, $limit, null);
+		}
+
+		$select->where('items.collection_id = ?', $id);
+		return $this->fetchObjects($select);
+	}
+
+	/**
+	 * Finds the most recent items from a given collection.
+	 *
+	 * @param  int $id  The collection id
+	 * @param  int $num The number of items to find
+	 * @return array    Result set
+	 */
+	public function findRecentInCollection($id, $limit = 10)
+	{
+		$params = array(
+			'sort_field' => 'added',
+			'sort_dir' => 'd'
+		);
+
+		$select = $this->getSelectForFindBy($params);
+		if ($limit) {
+			$this->applyPagination($select, $limit, null);
 		}
 
 		$select->where('items.collection_id = ?', $id);
